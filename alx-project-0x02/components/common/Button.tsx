8a1dc55f0c76de40d3e650ef22afd2cee251b936
Gedion47 <gedionalemu279@gmail.com>
2025-09-28 17:21:13 +0300

    updated

---
+++ alx-project-0x02/components/common/Button.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { type ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({
+  label,
+  onClick,
+  type = "button",
+  className = "",
+  size = "medium",
+  shape = "rounded-md",
+}) => {
+  return (
+    <div className="inline-block">
+      <button
+        type={type}
+        onClick={onClick}
+        className={`
+          bg-gray-50 hover:bg-green-50
+          text-gray-700 hover:text-green-700
+          border border-gray-200 hover:border-green-300
+          px-4 py-1.5
+          rounded-full
+          text-xs
+          transition-colors duration-200 font-semibold
+          ${
+            size === "small"
+              ? "text-xs"
+              : size === "large"
+              ? "text-lg"
+              : "text-sm"
+          }
+          ${className}
+          ${
+            shape === "rounded-sm"
+              ? "rounded-sm"
+              : shape === "rounded-full"
+              ? "rounded-full"
+              : "rounded-md"
+          }
+        `}
+      >
+        {label}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import Button from "@/components/common/Button";
+
 export const About: React.FC = () => (
-  <div className="p-8 bg-white rounded-xl shadow-lg mt-8">
-    <h2 className="text-3xl font-bold text-gray-800 mb-4">About Our Project</h2>
-    <p className="text-gray-600">
-      This content is rendered by the **About** component. Clicking the About
-      link updates the application state to show this component.
-    </p>
-  </div>
+  <>
+    <div className="p-8 bg-white rounded-xl shadow-lg mt-8">
+      <h2 className="text-3xl font-bold text-gray-800 mb-4">
+        About Our Project
+      </h2>
+      <p className="text-gray-600">
+        This content is rendered by the **About** component. Clicking the About
+        link updates the application state to show this component.
+      </p>
+    </div>
+    <Button label="Go Back" onClick={() => {}} />
+    <Button
+      label="Click Me"
+      onClick={() => alert("Button Clicked!")}
+      className="bg-blue-500 text-white"
+    />
+    <Button label="Submit" type="submit" className="bg-green-500 text-white" />
+    <Button label="Reset" type="reset" className="bg-red-500 text-white" />
+  </>
 );
 export default About;
