44dc46769cacb658d533ffddde280a0f798c0578
Gedion47 <gedionalemu279@gmail.com>
2025-09-28 16:53:47 +0300

    updated

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,1 +1,9 @@
-/* index */
+export interface CardProps {
+  title: string;
+  location: string;
+  price: string;
+  rating: number;
+  content: string[];
+  imageUrl: string;
+  iconsUrl: string[];
+}
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,11 +1,28 @@
 import React from "react";
+import Card from "@/components/common/Card";
+
 export const Home: React.FC = () => (
-  <div className="p-8 bg-white rounded-xl shadow-lg mt-8">
-    <h2 className="text-3xl font-bold text-gray-800 mb-4">Welcome Home!</h2>
-    <p className="text-gray-600">
-      This content is rendered by the **Home** component. The header link
-      directly controls whether this component is displayed.
-    </p>
+  <div>
+    <Card
+      title="Beautiful Apartment"
+      location="New York, NY"
+      price="$120"
+      rating={4.5}
+      content={["Top Villa", "Self Checkin", "Free Reschedule"]}
+      imageUrl="/assets/listing images/List 1.svg"
+      iconsUrl={[
+        "/assets/Icons/bed 1.svg",
+        "/assets/Icons/bathtub 1.svg",
+        "/assets/Icons/people 1.svg",
+      ]}
+    />
+    <div className="p-8 bg-white rounded-xl shadow-lg mt-8">
+      <h2 className="text-3xl font-bold text-gray-800 mb-4">Welcome Home!</h2>
+      <p className="text-gray-600">
+        This content is rendered by the **Home** component. The header link
+        directly controls whether this component is displayed.
+      </p>
+    </div>
   </div>
 );
 export default Home;
