32e883222a3b724d4b795b5a5110bc113d2fc603
Gedion47 <gedionalemu279@gmail.com>
2025-09-28 17:08:19 +0300

    updated

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,3 +1,21 @@
+export interface PostProps {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+export interface UserProps {
+  id: number;
+  name: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+  };
+}
 export interface CardProps {
   title: string;
   location: string;
@@ -6,4 +24,37 @@ export interface CardProps {
   content: string[];
   imageUrl: string;
   iconsUrl: string[];
+}
+
+export interface ButtonProps {
+  label: string;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+  className?: string;
+  size?: "small" | "medium" | "large";
+  shape?: "rounded-sm" | "rounded-md" | "rounded-full";
+}
+
+export interface PropertyProps {
+  name: string;
+  address: {
+    state: string;
+    city: string;
+    country: string;
+  };
+  rating: number;
+  category: string[];
+  price: number;
+  offers: {
+    bed: string;
+    shower: string;
+    occupants: string;
+  };
+  image: string;
+  discount: string;
+}
+export interface ModalProps {
+  isopen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
 }
